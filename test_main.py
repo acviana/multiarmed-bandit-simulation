ea56477a0e0d6e65db5db9e1e5f9f705db4bbb21
Alex C. Viana <alexcostaviana@gmail.com>
2024-07-23 15:37:51 -0500

    Cleaned up tests to match new code and added test for incremental_mean formula

--- test_main.py
+++ test_main.py
@@ -1,6 +1,12 @@
 import pytest
 
-from main import Bandit, TestBed
+from main import incremental_mean, Bandit, TestBed
+
+
+def test_incremental_mean():
+    assert incremental_mean(mean=0.0, observation=1.0, n=2) == 0.5
+    assert incremental_mean(mean=0.5, observation=1.0, n=3) == 2.0 / 3.0
+    assert incremental_mean(mean=(2.0 / 3.0), observation=2.0, n=4) == 1.0
 
 
 class TestBandit:
@@ -10,20 +16,22 @@ class TestBandit:
     def test_init(self):
         assert len(self.bandit) == 0
         assert len(self.bandit.observations) == 0
-        assert len(self.bandit.observed_means) == 0
+        assert len(self.bandit.means) == 0
         with pytest.raises(IndexError):
-            assert self.bandit.latest_mean == 0
+            assert self.bandit.means[-1] == 0
 
     def test_first_step(self):
         self.bandit.step()
         assert len(self.bandit) == 1
         assert len(self.bandit.observations) == 1
-        assert len(self.bandit.observed_means) == 1
+        assert len(self.bandit.means) == 1
 
 
 class TestTestBed:
     def setup_method(self):
-        self.test_bed = TestBed([Bandit(mean=0, stdev=1) for _ in range(10)])
+        self.test_bed = TestBed(
+            bandits=[Bandit(mean=0, stdev=1) for _ in range(10)], default_mean=0
+        )
 
     def test_best_bandit(self):
         self.test_bed.best_bandit()
